8f26432672b5f3b5b864224d65f53edd5599cc88
ggegoge <33847650+test0wanie@users.noreply.github.com>
2020-09-16 11:50:46 +0200

    cleaner main.c, run.c does the printing

--- main.c
+++ main.c
@@ -2,57 +2,17 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include "expressions.h"
-#include "onp.h"
+#include "run.h"
 
-/*
-Programik analizujacy wyrazenia matematyczne oraz
-(jezeli sa liczbowe) obliczajacy ich wartosc. Zapisuje
-je w strukturze drzewa i nastepnie umie wypisac NP, ONP
-oraz po prostu.
-przyklady:
-    symbolny: a*(b/c+d)-(e+f)*g
-    liczebny: 2*(21/7+4)-(5+2)*2
-*/
-
-int main(void) {
-  node* root;
+int main(int argc, char** argv) {
   char wyr[40];
   
-  printf("Podaj wyrazenie matematyczne:\n");
-  scanf("%s", wyr);
-
-  /* tworze drzewo */
-  root = expr(wyr, 0, strlen(wyr));
-
-  if (!if_nums(root)) {                    /* wariant symboliczny */
-    printf("Lukasiewicz:\n");
-    np(root);
-    printf("\n");
-    
-    printf("Azciweisakul:\n");
-    onp(root);
-    printf("\n");
-    
-    printf("reminder, ze bylo to:\n");
-    normalna(root);
-    printf("\n\n");
+  if (argc < 2) {
+    printf("Podaj wyrazenie matematyczne:\n");
+    scanf("%s", wyr);
+    run(wyr, strlen(wyr));
   }
-  else {                                   /* wariant liczbowy */    
-    printf("Lukasiewicz:\n");
-    np_filled(root);
-    printf("\n");
-    
-    printf("Azciweisakul:\n");
-    onp_filled(root);
-    printf("\n");
-    
-    printf("reminder, ze bylo to:\n");
-    normalna_filled(root);
-    printf("= %d\n\n", eval(root));
-  }
-  /* clean up */
-  delete_tree(&root);
-  
+  else 
+    run(argv[1], strlen(argv[1]));
   return 0;
 }
